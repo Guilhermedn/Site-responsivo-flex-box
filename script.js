3e2eeaa38bd121f53052d8c7449875a92f43ecca
Guilherme Alves <guilhermealves38@hotmail.com>
2021-07-14 10:24:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.querySelector(".menumobile").addEventListener("click", function() {
+    if(document.querySelector(".menu nav ul").style.display == 'flex') {
+        document.querySelector(".menu nav ul").style.display = 'none'
+    }
+    else {
+        document.querySelector(".menu nav ul").style.display = 'flex'
+    }
+ 
+    })
+}
